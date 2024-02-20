2b9530303fd23af9a422afeae3110791226b3f4c
Patrickpisana23 <patrickpisana@unmsm.edu.pe>
2024-02-20 17:44:20 -0500

    modificado

--- analysis.py
+++ analysis.py
@@ -12,3 +12,5 @@ st.write("""
 Este Proyecto se enfoca en el analisis de transporte publico de una empresa X en la cual se vera la cantidad de combustible, 
          los kilometros recorridos y muchos otros campos mas...
          """)
+
+st.image("imagen.jpg")
